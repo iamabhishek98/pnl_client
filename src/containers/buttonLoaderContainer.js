c8f9bb7956b4a91ddf1ae2bb4a1b011402b04519
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2020-07-15 11:57:41 +0800

    added button loader container

---
+++ src/containers/buttonLoaderContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ButtonLoaderContainer = ({ onButtonSubmit, text, color, loading }) => {
+  const button_css = "w3-button w3-round react_button" + " w3-" + color;
+
+  return (
+    <span>
+      <button
+        onClick={onButtonSubmit}
+        className={button_css}
+        disabled={loading}
+      >
+        {loading && (
+          <span>
+            <i className="fa fa-refresh fa-spin"></i>&nbsp;
+          </span>
+        )}
+        {text}
+      </button>
+    </span>
+  );
+};
+
+export default ButtonLoaderContainer;
